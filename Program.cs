0d3d5a4060393c36b890a9f4b116372d0fe0a73e
KevinLivora <kevinlivora23@gmail.com>
2024-04-19 19:55:20 -0300

    Corrección de identado

--- Program.cs
+++ Program.cs
@@ -1,20 +1,20 @@
 ﻿//Hacer un programa donde se ingrese la fecha actual y compruebe si corresponde al día de tu cumpleaños. Mostrar un mensaje de felicitaciones en tal caso.
 
 int diaCumple = 23; 
-        int mesCumple = 7;  
+int mesCumple = 7;  
 
-        Console.WriteLine("Ingrese la fecha actual:");
-        Console.Write("Día: ");
-        int diaActual = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ingrese la fecha actual:");
+Console.Write("Día: ");
+int diaActual = Convert.ToInt32(Console.ReadLine());
 
-        Console.Write("Mes: ");
-        int mesActual = Convert.ToInt32(Console.ReadLine());
+Console.Write("Mes: ");
+int mesActual = Convert.ToInt32(Console.ReadLine());
 
-        if (diaActual == diaCumple && mesActual == mesCumple)
-        {
-            Console.WriteLine("Feliz cumpleaños!");
-        }
-        else
-        {
-            Console.WriteLine("Hoy no es tu cumpleaños.");
-        }
+if (diaActual == diaCumple && mesActual == mesCumple)
+{
+    Console.WriteLine("Feliz cumpleaños!");
+}
+else
+{
+    Console.WriteLine("Hoy no es tu cumpleaños.");
+}
